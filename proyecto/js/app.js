d0c558245149dc816e14e77dd4b275189f0ea65e
andres-d-garcia <andresdavidgarciasilva.06@gmail.com>
2026-05-30 22:35:24 -0400

    Agregado de estructura de carpetas inicial, y constantes generales de .js

---
+++ proyecto/js/app.js
@@ -0,0 +1,9 @@
+const gameState = {
+    currentMode: 'solitario',    // 'solitario', 'pvp', 'libre'
+    difficulty: 'facil',        // 'facil', 'intermedio', 'dificil'
+    currentTheme: 'animales',    // El tema seleccionado
+    boardDimensions: { rows: 4, cols: 4 }, // Se actualiza según la dificultad
+    isBoardLocked: false,        // Bandera para bloquear clics durante el setTimeout de error
+    totalPairs: 8,               // Cantidad de pares totales a encontrar (mitad del tablero)
+    pairsFound: 0                // Contador de pares descubiertos hasta el momento
+};
---
+++ proyecto/js/game.js
@@ -0,0 +1,4 @@
+// En game.js
+let firstCardSelected = null;  // Almacena el elemento del DOM de la primera carta volteada
+let secondCardSelected = null; // Almacena el elemento del DOM de la segunda carta volteada
+let cardsFlippedThisTurn = 0;   // Contador para controlar que no volteen más de 2 cartas a la vez
